29cab65745f4e8283ebae7e631c9ec1d074955d3
Sherlock <sherlockliao01@gmail.com>
2017-06-18 17:36:22 +0800

    update

---
+++ 08-AutoEncoder/simple_autoencoder.py
@@ -0,0 +1,85 @@
+__author__ = 'SherlockLiao'
+
+import torch
+import torchvision
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.utils import save_image
+from torchvision.datasets import MNIST
+
+
+def to_img(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+
+num_epochs = 100
+batch_size = 128
+learning_rate = 1e-3
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+dataset = MNIST('./data', transform=img_transform)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28*28, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 3)
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 28*28),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+model = autoencoder().cuda()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
+                             weight_decay=1e-5)
+
+for epoch in range(num_epochs):
+    for data in dataloader:
+        img, _ = data
+        num_img = img.size(0)
+        img = img.view(num_img, -1)
+        img = Variable(img).cuda()
+        # ===================forward=====================
+        output = model(img)
+        loss = criterion(output, img)
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # ===================log========================
+    print('epoch [{}/{}], loss:{:.4f}'
+          .format(epoch+1, num_epochs, loss.data[0]))
+    if epoch % 10 == 0:
+        pic = to_img(output.cpu().data)
+        save_image(pic, './img/image_{}.png'.format(epoch))
