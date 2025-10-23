a57dd7ea35c9965a41f4963dee62d38884f92a65
viraj sharma <virajsharma1501@gmail.com>
2025-10-23 14:53:39 +0530

    Merge pull request #43 from virajsharma2000/performance-optimize-in-pytorch-tutorial
    removed unnecessary conversions of tensors in linear regression code

--- 01-Linear Regression/Linear_Regression.py
+++ 01-Linear Regression/Linear_Regression.py
@@ -5,23 +5,17 @@
 """
 
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
 from torch import nn
 from torch.autograd import Variable
 
-x_train = np.array([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168],
+x_train = torch.tensor([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168],
                     [9.779], [6.182], [7.59], [2.167], [7.042],
-                    [10.791], [5.313], [7.997], [3.1]], dtype=np.float32)
+                    [10.791], [5.313], [7.997], [3.1]], dtype=torch.float32)
 
-y_train = np.array([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573],
+y_train = torch.tensor([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573],
                     [3.366], [2.596], [2.53], [1.221], [2.827],
-                    [3.465], [1.65], [2.904], [1.3]], dtype=np.float32)
-
-
-x_train = torch.from_numpy(x_train)
-
-y_train = torch.from_numpy(y_train)
+                    [3.465], [1.65], [2.904], [1.3]], dtype=torch.float32)
 
 
 # Linear Regression Model
